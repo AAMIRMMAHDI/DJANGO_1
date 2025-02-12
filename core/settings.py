6e4bf3e23c820a75c2ce85e3fa68c537622ac38f
AAMIRMMAHDI <156841411+AAMIRMMAHDI@users.noreply.github.com>
2025-02-12 18:56:50 +0330

    a

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-qc&ffu#0$^+2allr1lsu^@h#nldqi8_i&8#ybq#(u%lzn*#qn!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.5', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 INSTALLED_APPS = [
